2f871f3b0da0d492d63a97543499570abd622355
Della Duck <della.duck@outlook.be>
2025-05-26 16:43:21 +0200

    Exemple code JS 🖥

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log('Ceci est un fichier JS !');
+
+function filter(array, callback) {
+    const result = [];
+    for(const elem of array) {
+        if(callback(elem)) {
+            result.push(elem);
+        }
+    }
+    return result;
+}
